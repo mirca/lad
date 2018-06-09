d7c5107561bcd018cc7e05675f104e1a6e344a1c
mirca <jvmirca@gmail.com>
2018-06-09 13:59:13 -0300

    minor tweaks

--- lad/lad.py
+++ lad/lad.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import math
+import numpy as np
 
-def lad(X, y, yerr=None, l1_regularizer=0.12, maxiter=5, rtol=1e-4,
+def lad(X, y, yerr=None, l1_regularizer=0.5, maxiter=50, rtol=1e-4,
         eps=1e-4, session=None):
     """
     Linear least absolute deviations with L1 norm regularization using
@@ -50,15 +51,16 @@ def lad(X, y, yerr=None, l1_regularizer=0.12, maxiter=5, rtol=1e-4,
     X_tensor = tf.convert_to_tensor((X.T / whitening_factor).T, dtype=tf.float64)
     y_tensor = tf.reshape(tf.convert_to_tensor(y / whitening_factor,
                                                dtype=tf.float64), (-1, 1))
+    eps = tf.convert_to_tensor(eps, dtype=tf.float64)
 
     with session or tf.Session() as session:
-        # solves the L2 regularization problem and
-        # use it as initial values for the MM algorithm
+        # solves the L2 norm with L2 regularization problem
+        # and use its solution as initial value for the MM algorithm
         x = tf.matrix_solve_ls(X_tensor, y_tensor, l2_regularizer=l1_regularizer)
         n = 0
         while n < maxiter:
             reg_factor = tf.norm(x, ord=1)
-            l1_factor = eps + tf.sqrt(tf.abs(y_tensor - tf.matmul(X_tensor, x)))
+            l1_factor = tf.maximum(eps, tf.sqrt(tf.abs(y_tensor - tf.matmul(X_tensor, x))))
 
             X_tensor = X_tensor / l1_factor
             y_tensor = y_tensor / l1_factor
@@ -67,7 +69,7 @@ def lad(X, y, yerr=None, l1_regularizer=0.12, maxiter=5, rtol=1e-4,
             xo = tf.matrix_solve_ls(X_tensor, y_tensor,
                                     l2_regularizer=l1_regularizer/reg_factor)
 
-            rel_err = tf.norm(x - xo, ord=1) / (1. + reg_factor)
+            rel_err = tf.norm(x - xo, ord=1) / tf.maximum(tf.constant(1., dtype=tf.float64), reg_factor)
             if session.run(rel_err) < rtol:
                 break
             x = xo
--- lad/tests/test_lad.py
+++ lad/tests/test_lad.py
@@ -4,16 +4,17 @@ from ..lad import lad
 
 
 def test_least_absolute_deviations():
+    sess = tf.Session()
     m_true = 3
     b_true = 10
-    x = np.linspace(0, 10, 200)
-    y = x * m_true + b_true + np.random.normal(scale=2, size=x.shape)
-
+    x = np.linspace(0, 10, 10000)
+    y = x * m_true + b_true + np.random.laplace(scale=1., size=x.shape)
     X = np.vstack([x, np.ones(len(x))])
 
-    coeffs = lad(X.T, y)
-    sess = tf.Session()
-    m, b = sess.run(coeffs)
+    m, b = sess.run(lad(X.T, y))
+    assert abs(m - m_true)/m_true < 1e-2
+    assert abs(b - b_true)/b_true < 1e-2
 
-    assert abs(m - m_true)/m_true < 1e-1
-    assert abs(b - b_true)/b_true < 1e-1
+    m, b = sess.run(lad(X.T, y, yerr=np.std(y)))
+    assert abs(m - m_true)/m_true < 1e-2
+    assert abs(b - b_true)/b_true < 1e-2
