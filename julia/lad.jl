892765e76644b53a3650083f74461f8fb5c744bd
mirca <jvmirca@gmail.com>
2018-07-19 08:38:54 -0300

    working on julia version

--- julia/lad.jl
+++ julia/lad.jl
@@ -1,4 +1,4 @@
-function lad(X, y, yerr=nothing, l1_regularizer=0., maxiter=50, rtol=1e-4, eps=1e-4)::Array{Float64, 2}
+function lad(X, y, yerr=nothing, l1regularizer=0., maxiter=50, rtol=1e-4, eps=1e-4)::Array{Float64, 2}
 
     if yerr != nothing
         yerr /= sqrt(2.)
@@ -10,17 +10,26 @@ function lad(X, y, yerr=nothing, l1_regularizer=0., maxiter=50, rtol=1e-4, eps=1
     y /= yerr
 
     p = size(X)[2]
-    beta = solve(X, y, w, p)
+    l1regularizer = diag(l1regularizer, p)
+    beta = solve(X, y, l1regularizer, p)
     regfactor = norm(beta, 1.)
-    lambda = diag(l1_regularizer, p)
     k = 1
     while k <= maxiter
-        l1factor = vec(eps + sqrt(abs(y - X' * beta)))
-        beta_k = solve(X, y, )
+        l1factor = vec(eps + sqrt(abs(y - X * beta)))
+        beta_k = solve(X/l1factor, y/l1factor, l1regularizer, p)
+        relerr = norm(beta_k - beta, 1) / max(1., regfactor)
+
+        if relerr < rtol
+            break
+        end
+        beta = beta_k
+        regfactor = norm(beta, 1.)
+        k += 1
     end
 
+    return beta
 end
 
-function solve(X, y, w)
+function solve(X, y, w, p)
     return (cholfact(X' * X + w) \ ones{Float64, (p, 1)}) * X' * y
 end
