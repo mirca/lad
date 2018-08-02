fbf7d3d93108146d43e2df34bd55f9ede462e335
mirca <jvmirca@gmail.com>
2018-08-01 21:58:54 -0300

    add julia version

--- julia/Lad.jl/src/Lad.jl
+++ julia/Lad.jl/src/Lad.jl
@@ -1,3 +1,5 @@
+module Lad
+
 function lad(X, y, yerr=nothing, l1regularizer=0., maxiter=50, rtol=1e-4, eps=1e-4)::Array{Float64, 2}
 
     if yerr != nothing
@@ -10,13 +12,13 @@ function lad(X, y, yerr=nothing, l1regularizer=0., maxiter=50, rtol=1e-4, eps=1e
     y /= yerr
 
     p = size(X)[2]
-    l1regularizer = diag(l1regularizer, p)
+    l1regularizer = Diagonal(eye(p) * l1regularizer)
     beta = solve(X, y, l1regularizer, p)
     regfactor = norm(beta, 1.)
     k = 1
     while k <= maxiter
-        l1factor = vec(eps + sqrt(abs(y - X * beta)))
-        beta_k = solve(X/l1factor, y/l1factor, l1regularizer, p)
+        l1factor = vec(eps + sqrt.(abs.(y - X * beta)))
+        beta_k = solve(X./l1factor, y./l1factor, l1regularizer, p)
         relerr = norm(beta_k - beta, 1) / max(1., regfactor)
 
         if relerr < rtol
@@ -30,6 +32,8 @@ function lad(X, y, yerr=nothing, l1regularizer=0., maxiter=50, rtol=1e-4, eps=1e
     return beta
 end
 
-function solve(X, y, w, p)
-    return (cholfact(X' * X + w) \ ones{Float64, (p, 1)}) * X' * y
+function solve(X, y, W, p)
+    return (cholfact(X' * X + W) \ eye(p)) * X' * y
+end
+
 end
---
+++ julia/Lad.jl/test/runtests.jl
@@ -0,0 +1,12 @@
+using Lad
+using Base.Test
+
+n = 1000
+beta = [3. 10.]'
+X = rand(Float64, n, 1)
+X = [X ones(Float64, n, 1)]
+y = X * beta + randn(Float64, n, 1)
+
+beta_lad = Lad.lad(X, y)
+@test beta ≈ beta_lad rtol=1e-2
+
